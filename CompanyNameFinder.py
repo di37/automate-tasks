1ecbccb4b62d77b994d3f31133d661beb000b0de
Isham Rashik <d.isham.993@gmail.com>
2017-09-14 16:04:30 +0400

    Second version of both CompanyNameFinder.py and pdfdownloader.py are ready.

--- CompanyNameFinder.py
+++ CompanyNameFinder.py
@@ -4,13 +4,13 @@
     Author of this program: Doula Isham Rashik
     Date Published: 14/09/2017
 
-	Note: Only to be used for educational purposes.
+    Note: Only to be used for educational purposes.
 
-	Description: This piece of program allows to obtain the names of the companies
-	in UAE within a specific city from yellow pages.
+    Description: This piece of program allows to obtain the names of the companies
+    in UAE within a specific city from yellow pages.
 
-	Input: Keyword/Catagory, maximum number of pages to be scraped, City
-	Output: List of company names from specific city
+    Input: Keyword/Catagory, maximum number of pages to be scraped, City
+    Output: List of company names from specific city
 '''
 
 # Importing modules that will allow access to the webpages
@@ -26,32 +26,37 @@ def inputs():
     get_pages(search, max_pages, city)
 
 
-# Definition of function to enter the yellow page webpages 
+# Definition of function to enter the yellow page webpages and extracts company names
 def get_pages(keyword, max_pages, city):
-    for i in range(max_pages):
-        get_companies("http://yellowpages.ae/s/" + city + "/" + keyword + "-" + str(i) + "-1.html")
-
-
-# Definition of function that extracts the company names
-def get_companies(url):
-    r = requests.get(url)
-    soup = BeautifulSoup(r.content, 'lxml')
-    g_data = soup.find_all("section", {"class": "left"})
-
     companies = []
 
-    for item in g_data:
-        companies.append(item.contents[3].text.strip(' '))
+    for i in range(max_pages):
+        url = "http://yellowpages.ae/s/" + city + "/" + keyword + "-" + str(i) + "-1.html"
+        r = requests.get(url)
+        soup = BeautifulSoup(r.content, 'lxml')
+        g_data = soup.find_all("section", {"class": "left"})
 
-    print("")
-    # To print list of company names
-    for company in companies:
-        print(company)
-    print("")
+        for item in g_data:
+            companies.append(item.contents[3].text.strip(' '))
+
+    out = input("\nDisplay the companies in terminal[T/t] or store and save them in a file[S/s]? ")
+
+    if out == 'T' or out == 't':
+        print('\nList of companies in ' + city + ' as follows: \n\n')
+        for company in companies:
+            print(company)
+
+    elif out == 'S' or out == 's':
+        playFile = open(keyword + '-companies-' + city + '.txt', 'w')
+        playFile.write('\nList of companies in ' + city + ' as follows: \n\n')
+        for company in companies:
+            playFile.write(company + '\n')
+        playFile.close()
+
 
 # Definite loop to check if the user wants to obtain company names again
 while True:
     inputs()
-    prompt = input("Do you want to continue[Y/y] or quit[N/n]? ")
+    prompt = input("\nDo you want to continue[Y/y] or quit the program[N/n]? ")
     if prompt == "N" or prompt == "n":
         break
